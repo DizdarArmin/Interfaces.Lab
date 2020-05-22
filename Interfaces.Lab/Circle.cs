fa06e106d3cc1b8577b71b87394e8376218733aa
DizdarArmin <armin.dizdar@gmail.com>
2020-05-22 09:55:12 +0200

    Circle and rectangle

---
+++ Interfaces.Lab/Circle.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces.Lab
+{
+    class Circle : IDrawable
+    {
+        private double radius;
+
+        public Circle(double radius)
+        {
+            this.Radius = radius;
+
+        }
+        public void Draw()
+        {
+            double rIn = this.Radius - 0.4;
+            double rOut = this.Radius + 0.4;
+
+            for (double y = this.Radius; y >= -this.Radius; --y) 
+            {
+                for (double x = -this.Radius; x < rOut; x += 0.5) 
+                {
+                    double value = x * x + y * y;
+
+                    if (value >= rIn * rIn && value <= rOut * rOut)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private double Radius {
+            get
+            {
+                return this.radius;
+            }
+            set 
+            {
+                this.radius = value;
+            }
+        }
+    }
+}
---
+++ Interfaces.Lab/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces.Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Circle, radius : ");
+            var radius = int.Parse(Console.ReadLine());
+            IDrawable circle = new Circle(radius);
+            circle.Draw();
+
+            Console.WriteLine("Rectangle, width: ");
+            var width = int.Parse(Console.ReadLine());
+            Console.WriteLine("Rectangle, height: ");
+            var height = int.Parse(Console.ReadLine());
+            IDrawable rect = new Rectangle(width, height);
+            rect.Draw();
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
